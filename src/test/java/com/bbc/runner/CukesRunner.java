2cc4c2e68139d50e7998f45b00c3ec51195b10b6
Hasosoon <hasanemirr15@gmail.com>
2023-06-26 23:41:50 +0100

    Beginning

---
+++ src/test/java/com/bbc/runner/CukesRunner.java
@@ -0,0 +1,19 @@
+package com.bbc.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumber-report.html",
+        "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+        "rerun:target/rerun.txt"},
+        features = "src/test/resources/features",
+        glue = "com/bbc/step_definitions",
+        dryRun = false,
+        tags = "@wip"
+)
+public class CukesRunner {
+
+}
